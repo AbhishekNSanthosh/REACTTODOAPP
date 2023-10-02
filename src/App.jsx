65eac187203ba384140ed85eb2e73f39567446c6
Abhishek <you@example.com>
2023-10-02 23:40:27 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import bgImg from './assets/bg.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([
+        ...tasks,
+        {
+          id: tasks.length + 1,
+          text: newTask,
+          createdAt: new Date().toLocaleString(),
+          completed: false,
+        },
+      ]);
+      setNewTask('');
+    }
+  };
+
+  const markTaskAsDone = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: true } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const renderTasks = (completed) =>
+    tasks
+      .filter((task) => task.completed === completed)
+      .map((task) => (
+        <div key={task.id} className={`task ${completed ? 'completed' : ''}`}>
+          <span className="task-text">{task.text}</span>
+          <span className="task-date">{task.createdAt}</span>
+          {!completed && (
+            <button
+              className="mark-done-button"
+              onClick={() => markTaskAsDone(task.id)}
+            >
+              Mark as Done
+            </button>
+          )}
+        </div>
+      ));
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="container">
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="todowrapper">
+        <div className="titleBox">
+          <span className="title">LoveTodo</span>
+        </div>
+        <div className="inputBox">
+          <input type="text" className='todoinput' placeholder='Add a new task' />
+          <span className="material-symbols-outlined send" >
+            send
+          </span>
+        </div>
+        <div className="todoDisplayBox">
+          <div className="info">
+            <div className="infoWrapper">
+              <span className="infoItem">Created Tasks: </span><div className="createdList">1</div>
+            </div>
+            <div className="infoWrapper">
+              <span className="infoItem">Completed Tasks:</span><div className="createdList">1/1</div>
+            </div>
+          </div>
+          <hr />
+
+          <div className="todoItem">
+            <div className="todoItemWrapper">
+              <div className="checkbox">
+                <span class="material-symbols-outlined check">
+                  check_circle
+                </span>
+              </div>
+              <div className="todoDetails">
+                <span className="todoTitle">Morning Walk</span>
+                <span className="todoTime">21:09pm</span>
+              </div>
+              <div className="action">
+                <span class="material-symbols-outlined check">
+                  delete
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="love">
+        <span className="author">Made with <span className="emoji">❤️</span> Abhishek</span>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
