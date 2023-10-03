99f86d2945b6189b0a436fd4077446e89395f97d
Abhishek <you@example.com>
2023-10-03 22:18:52 +0530

    update1

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() { 
+function App() {
 
-const [todos,setTodos]=useState([])
-const [todo,setTodo] = useState('')
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState('')
 
-function HandleChange(e){
-  setTodo(e.target.value);
-  console.log(todo);
-}
-
-//add a todo
-function addTodo(){
-  const newTodo = {
-    id: todos.length+1,
-    title: todo,
-    date : new Date().toLocaleString(),
-    isComplete: false
+  function HandleChange(e) {
+    setTodo(e.target.value);
+    console.log(todo);
   }
-  setTodos([...todos,newTodo]);
-  setTodo("")
-  console.log("hello ",todos)
 
-}
+  //add a todo
+  function addTodo() {
+    const newTodo = {
+      id: todos.length + 1,
+      title: todo,
+      date: new Date().toLocaleString(),
+      isComplete: false
+    }
+    setTodos([...todos, newTodo]);
+    setTodo("")
+    console.log("hello ", todos)
+
+  }
 
   return (
     <div className="App">
@@ -34,50 +34,54 @@ function addTodo(){
           <span className="title">LoveTodo</span>
         </div>
         <div className="inputBox">
-          <input type="text" className='todoinput' value={todo} placeholder='Add a new task' onChange={(e)=>(HandleChange(e))} />
+          <input type="text" className='todoinput' value={todo} placeholder='Add a new task' onChange={(e) => (HandleChange(e))} />
           <span onClick={addTodo} className="material-symbols-outlined send"  >
             send
           </span>
         </div>
         <div className="todoDisplayBox">
-          <div className="info">
-            <div className="infoWrapper">
-              <span className="infoItem">Created Tasks: </span><div className="createdList">1</div>
-            </div>
-            <div className="infoWrapper">
-              <span className="infoItem">Completed Tasks:</span><div className="createdList">1/1</div>
-            </div>
-          </div>
-          <hr />
+          {todos.length != 0 &&
+            <>
+              <div className="info">
+                <div className="infoWrapper">
+                  <span className="infoItem">Created Tasks: </span><div className="createdList">1</div>
+                </div>
+                <div className="infoWrapper">
+                  <span className="infoItem">Completed Tasks:</span><div className="createdList">1/1</div>
+                </div>
+              </div>
+              <hr />
+            </>
+          }
 
           <div className="todoItemCol">
 
             {
-              todos && todos.map((task)=>(
+              todos && todos.map((task) => (
 
-<div className="todoItem">
-              <div className="todoItemWrapper">
-                <div className="checkbox">
-                  <span class="material-symbols-outlined check">
-                    check_circle
-                  </span>
+                <div className="todoItem">
+                  <div className="todoItemWrapper">
+                    <div className="checkbox">
+                      <span class="material-symbols-outlined check">
+                        check_circle
+                      </span>
+                    </div>
+                    <div className="todoDetails">
+                      <span className="todoTitle">{task.title}</span>
+                      <span className="todoTime">{task.date}</span>
+                    </div>
+                    <div className="action">
+                      <span className="material-symbols-outlined check">
+                        delete
+                      </span>
+                    </div>
+                  </div>
                 </div>
-                <div className="todoDetails">
-                  <span className="todoTitle">{task.title}</span>
-                  <span className="todoTime">{task.date}</span>
-                </div>
-                <div className="action">
-                  <span className="material-symbols-outlined check">
-                    delete
-                  </span>
-                </div>
-              </div>
-            </div>
 
               ))
             }
 
-            
+
 
           </div>
         </div>
