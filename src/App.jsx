215dbbfe423ffcc6a9e5ac1e6e6becdf806b70b4
Abhishek <you@example.com>
2023-10-04 19:44:36 +0530

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import nothingImage from './assets/nothing.png'
 
 function App() {
 
   const [todos, setTodos] = useState([])
-  const [todo, setTodo] = useState('')
+  const [todo, setTodo] = useState('');
+  const [todoStatus, setTodoStatus] = useState(false);
+
+  const generateUniqueId = () => {
+    // Generate a unique ID for each todo item (you can use a library like uuid)
+    return new Date().getTime().toString();
+  };
+
+  //to get todos that are stored in localstorage.
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem('todos')) || [];
+    setTodos(storedTodos)
+  }, [])
+
+  //to store todo in localstorage
+  // useEffect(() => {
+  //   localStorage.setItem('todos', JSON.stringify(todos));
+  // }, [todos]);
+
 
   function HandleChange(e) {
     setTodo(e.target.value);
@@ -14,24 +32,41 @@ function App() {
   //add a todo
   function addTodo() {
     const newTodo = {
-      id: todos.length + 1,
+      id: generateUniqueId(),
       title: todo,
       date: new Date().toLocaleString(),
-      isComplete: false
+      isComplete: todoStatus
     }
     setTodos([...todos, newTodo]);
+    localStorage.setItem('todos', JSON.stringify([...todos, newTodo]));
     setTodo("")
+    countAllTodos();
   }
 
   //get todo count
   const countAllTodos = () => todos.length;
 
   //get completed todo count 
-  const countCompletedTodos = () => todos.filter((todo) => todo.completed).length;
+  const countCompletedTodos = () => todos.filter((todo) => todo.isComplete).length;
 
-  const deleteTodo = (index) => {
-    const updatedTodos = todos.filter((_, i) => i !== index);
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
+    countCompletedTodos()
+  };
+
+  //update the status of the todo
+  const handleCompletion = (index) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === index) {
+        return { ...todo, isComplete: !todo.isComplete };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+    countCompletedTodos()
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
   };
 
   return (
@@ -44,12 +79,12 @@ function App() {
         </div>
         <div className="inputBox">
           <input type="text" className='todoinput' value={todo} placeholder='Add a new task' onChange={(e) => (HandleChange(e))} />
-          <button className='submitBtn' type='button' onClick={(e)=>{
+          <button className='submitBtn' type='button' onClick={(e) => {
             e.preventDefault()
             addTodo()
           }}> <span className="material-symbols-outlined send"  >
-            send
-          </span></button>
+              send
+            </span></button>
         </div>
         <div className="todoDisplayBox">
           {todos.length != 0 &&
@@ -85,17 +120,31 @@ function App() {
                     <div className="todoItem" key={task?.id}>
                       <div className="todoItemWrapper">
                         <div className="checkbox">
-                          <span className="material-symbols-outlined check">
-                            check_circle
-                          </span>
+                          {task?.isComplete
+                            ?
+                            <span onClick={(e) => {
+                              console.log("first")
+                              handleCompletion(task?.id)
+                            }} className="material-symbols-outlined check">
+                              check_circle
+                            </span>
+                            :
+                            <span onClick={(e) => {
+                              console.log("first")
+                              handleCompletion(task?.id)
+                            }} className="material-symbols-outlined check">
+                              radio_button_unchecked
+                            </span>
+                          }
                         </div>
                         <div className="todoDetails">
                           <span className="todoTitle">{task.title}</span>
                           <span className="todoTime">{task.date}</span>
                         </div>
                         <div className="action">
-                          <span className="material-symbols-outlined check" onClick={() => {
-                            deleteTodo(task.id)
+                          <span className="material-symbols-outlined check" onClick={(e) => {
+                            e.preventDefault()
+                            deleteTodo(task?.id)
                           }}>
                             delete
                           </span>
